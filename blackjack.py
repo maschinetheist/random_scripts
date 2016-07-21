1c9d38e3ff7cf2f472a6a82f675ff8f596602e1e
mike <robot999@gmail.com>
2016-07-21 18:46:16 +0000

    Added basic point checking, still not done

--- blackjack.py
+++ blackjack.py
@@ -2,10 +2,7 @@
 
 import random
 import sys
-from fabulous.color import magenta, red, blue, blue_bg, red_bg, white
-
-rounds = 5
-points = 100
+from fabulous.color import magenta, red, blue, blue_bg, red_bg, white, yellow
 
 class CardDeck(object):
     def __init__(self):
@@ -32,7 +29,6 @@ class CardDeck(object):
         
         r_cardsuit = random.choice(card_suits)
         r_cardrank = random.choice(card_ranks.keys())
-        #r_cardrank, r_cardvalue = random.choice(card_ranks.items())
         if r_cardrank.startswith('rank_card'):
             card = r_cardrank, r_cardsuit, card_ranks[r_cardrank] #, random.randint(2,10)
         elif r_cardrank == 'ace':
@@ -45,6 +41,9 @@ class CardDeck(object):
 
 
 def main():
+    dollar_amt = 300
+    rounds = 0
+
     cards = CardDeck()
 
     def prompt(question):
@@ -53,14 +52,28 @@ def main():
         if reply[0] == 'y':
             return True
         if reply[0] == 'n':
-            sys.exit(1)
+            return False
         else:
             return prompt("Please answer [y|n]")
+
+    def make_bet():
+        while True:
+            bet = raw_input(" ")
+            try: 
+                return int(bet)
+            except ValueError: 
+                print "Enter a real number, fool."
 
     print blue("\nDraw a hand?")
     reply = str(raw_input("[y/n]:")).lower().strip()
     if reply[0] == 'y':
         while True:
+            rounds += 1
+            print yellow("Round #: " + str(rounds))
+            print "Enter a bet:"
+
+            bet = make_bet()
+
             print magenta("\nDrawing a random card.")
             dealt_card_1 = cards.deal_card()
             print dealt_card_1
@@ -71,16 +84,30 @@ def main():
 
             while True:
                 if dealt_hand == 21:
-                    print blue_bg(white("Blacjack!"))
+                    print blue_bg(white("Blacjack!\n"))
+                    dollar_amt = dollar_amt + ( bet * 2 )
+                    print "You won: " + str(bet * 2)
+                    print "Cash on hand: " + str(dollar_amt)
                     break
                 elif dealt_hand <= 21:
-                    prompt("Play another card? Please enter Y or N.")
-                    another_card = cards.deal_card()
-                    dealt_hand = dealt_hand + another_card[2]
-                    print magenta("\nDrawing another card:")
-                    print str(another_card)
+                    answer = prompt("Play another card? Please enter Y or N.") 
+                    if answer is True:
+                        another_card = cards.deal_card()
+                        dealt_hand = dealt_hand + another_card[2]
+                        print magenta("\nDrawing another card:")
+                        print str(another_card)
+                    elif answer is False:
+                        break
+                    #if prompt("Play another card? Please enter Y or N.") is False:
+                    #    break
+                    else:
+                        prompt("Please answer [y|n]")
                 elif dealt_hand >= 21:
-                    print red_bg(white("Busted!"))
+                    dollar_amt = dollar_amt - bet
+                    print "You lose: " + str(bet)
+                    print "Cash on hand: " + str(dollar_amt)
+
+                    print red_bg(white("Busted!\n"))
                     break
 
                 print dealt_hand
