1bdaf15ed9276e9f86f36f8ebd11c9109e529644
maschinetheist <robot999@gmail.com>
2016-10-25 08:55:52 -0500

    Added ssh-agent support

--- ssh_wrapper.py
+++ ssh_wrapper.py
@@ -19,7 +19,7 @@ def ssh_op(host, username, password, port, command):
 
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(hostname, username=username, password=password)
+	ssh.connect(hostname, username=username, password=password, allow_agent=True)
 
 	''' Run some commands '''
 	stdin, stdout, stderr = ssh.exec_command(command)
@@ -27,17 +27,25 @@ def ssh_op(host, username, password, port, command):
 		print ''.join(line.encode('ascii', 'ignore').split())
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="SSH Wrapper")
-	parser.add_argument('-H', action='append', dest='hosts', nargs='+', help="Hosts")
+	parser = argparse.ArgumentParser(description="SSH Wrapper that runs commands against hosts in parallel")
+	parser.add_argument('-H', action='append', dest='hosts', nargs='+', help="Hosts", required=True)
 	parser.add_argument('-P', action='store_true', dest='parallel', help="Parallel processing")
 	parser.add_argument('-u', action='store', dest='username', help="username")
 	parser.add_argument('-p', action='store', dest='port', help="SSH port")
-	parser.add_argument('-a', action='store', dest='command', help="remote command")
+	parser.add_argument('-k', action='store_true', dest='pki', help="Use ssh-agent and SSH key authentication")
+	parser.add_argument('-a', action='store', dest='command', help="remote command", required=True)
 	parser.add_argument('-v', action='version', version='%(prog)s 1.0', help="Prints a version number")
 	results = parser.parse_args()
 
+	if not vars(results):
+		parser.print_help()
+		sys.exit(0)
+
 	if results.username:
 		password = getpass.getpass()
+	if results.pki:
+		username = getpass.getuser()
+		password = None
 	else:
 		# get password from current running user
 		username = getpass.getuser()
