7bfa16083ef7f577a6fef7c76525b2accc6739c8
maschinetheist <robot999@gmail.com>
2016-10-27 10:46:50 -0500

    Added support for multiple hosts and reordered arguments

--- netdiff.py
+++ netdiff.py
@@ -31,8 +31,10 @@ def sftp_op(host, username, password, port, remote_file, local_path):
         sftp.get(remote_file, local_path)
     except paramiko.BadHostKeyException:
         print "SSH Host Key could not be verified"
+        sys.exit(1)
     except paramiko.AuthenticationException:
         print "Authentication failed"
+        sys.exit(1)
     except (paramiko.SSHException, socket.gaierror):
         print "Could not connect to remote host {hostname}".format(
             hostname=hostname)
@@ -48,11 +50,11 @@ def compare(local_file, local_path, remote_file):
     local_file_contents = None
 
     with open(remote_file, 'r') as rf:
-        remote_file_contents = rf.readline()
+        remote_file_contents = rf.read()
     rf.close()
 
     with open(local_file, 'r') as lf:
-        local_file_contents = lf.readline()
+        local_file_contents = lf.read()
     lf.close()
 
     local_file_contents = local_file_contents.splitlines()  
@@ -63,47 +65,57 @@ def compare(local_file, local_path, remote_file):
     print '\n'.join(list(diff))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Diff local files against remote files")
-    parser.add_argument('-H', action='store', nargs='+', dest='hosts', required=True)
-    parser.add_argument('-u', action='store', dest='username', help="Username")
-    parser.add_argument('-p', action='store', dest='port', help="SSH port")
+    parser = argparse.ArgumentParser(description="Diff local files against files on remote hosts")
+    #parser.add_argument('-H', action='store', nargs='+', dest='hosts')
+    #parser.add_argument('-u', action='store', dest='username', help="Username")
+    parser.add_argument('-p', '--port', action='store', dest='port', help="SSH port")
     parser.add_argument('-k', action='store_true', dest='pki', help="Use ssh-agent and SSH key authentication")
-    parser.add_argument('files', nargs='*') #, action='append')
-    parser.add_argument('-v', action='version', version='%(prog)s 1.1', help="Prints a version number")
+    parser.add_argument('local_file', nargs=1, help="local file")
+    parser.add_argument('remote_file', nargs='*', help="[USER@]HOST:file")
+    parser.add_argument('-v', action='version', version='%(prog)s 1.3', help="Prints a version number")
     results, extras = parser.parse_known_args()
-    results.files.extend(extras)
+    results.local_file.extend(extras)
+    results.remote_file.extend(extras)
 
     if not vars(results):
         parser.print_help()
         sys.exit(0)
+    
+    if results.pki:
+    # force ssh pubkey auth
+        password = None
+    else:
+    # ask for password
+        password = getpass.getpass()
 
-    if results.hosts: 
-        if results.username:
-            username = results.username
-            password = getpass.getpass()
-        elif results.pki:
-            username = getpass.getuser()
-            password = None
+    if not results.port:
+        port = 22
+
+    for remote_uri in results.remote_file:
+        print remote_uri + ":"
+
+    #    for host in results.hosts:
+        local_file = ''.join(results.local_file)
+        remote_uri = ''.join(remote_uri)
+
+        if "@" in remote_uri:
+            username = remote_uri.split("@")[0]
+            remote_uri = remote_uri.split("@")[1]
         else:
+        # if we didn't supply username in uri, get local username
             username = getpass.getuser()
-            password = getpass.getpass()
+        host = remote_uri.split(":")[0]
+        remote_file = remote_uri.split(":")[1]
 
-        if not results.port:
-            port = 22
+        try:
+            # Need to create a named temporary file. It'll be removed
+            # when we close it. Make sure to use local_path.name as we
+            # can't reference an instance object of the temporary file.
+            local_path = tempfile.NamedTemporaryFile()
 
-        for host in results.hosts:
-            print results.files
-            local_file = results.files[0]
-            remote_file = results.files[1]
-
-            try:
-                # Need to create a named temporary file. It'll be removed
-                # when we close it. Make sure to use local_path.name as we
-                # can't reference an instance object of the temporary file.
-                local_path = tempfile.NamedTemporaryFile()
-
-                sftp_op(host, username, password, port, remote_file, local_path.name)
-                local_path.seek(0)
-                compare(local_file, local_path.name, remote_file)
-            finally:
-                local_path.close()
+            sftp_op(host, username, password, port, remote_file, local_path.name)
+            local_path.seek(0)
+            compare(local_file, local_path.name, remote_file)
+            print ''
+        finally:
+            local_path.close()
