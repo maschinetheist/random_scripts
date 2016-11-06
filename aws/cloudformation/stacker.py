63b3bc071ffc8c29c61ef60139c05a71e44c0b36
Mike <mike@project6209.com>
2016-11-06 17:53:05 -0600

    Added argparse, needs testing

--- aws/cloudformation/stacker.py
+++ aws/cloudformation/stacker.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 from boto import (cloudformation, s3, exception)
 import argparse
 import time
+import sys
 
 class Stacker():
     def __init__(self, region):
@@ -44,6 +45,13 @@ class Stacker():
             print(stack.stack_name)
 
     def upload_template(self, local_template):
+        '''
+        Upload template file to S3 bucket to be parsed by the CF create function.
+
+        :param str self.bucket              S3 bucket name
+        :param str self.local_template      local template file
+        :param str self.template            template name without the path
+        '''
         self.bucket = 'cf-templates-158gc87u1eu1y-us-east-1'
         self.local_template = local_template
         self.template = self.local_template.replace('/home/mike/', '')
@@ -61,6 +69,21 @@ class Stacker():
         finally:
             template_url = "http://s3.amazonaws.com/" + self.bucket + "/" + self.template
         return template_url
+
+    def local_template(self, local_template):
+        '''
+        Return template body from a template file.
+
+        :param str self.local_template      template file location
+        :param str self.template            template body object
+        '''
+        self.local_template = local_template
+        self.template = template
+
+        with open(self.local_template, 'r') as f:
+            self.template = f.readline()
+        f.close
+        return self.template
 
     def stack_status(self, stack_name):
         self.stack_name = stack_name
@@ -102,9 +125,32 @@ class Stacker():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="AWS CloudFormation CLI tool")
+    parser.add_argument('-c', '--create', action='store', dest='create', help="Create stack using a template")
+    parser.add_argument('-d', '--delete', action='store', dest='delete', help="Delete a given stack")
+    parser.add_argument('-l', '--list', action='store', dest='list', help="List existing stacks")
+    parser.add_argument('-t', '--template', action='store', dest='template_file', help="Location of the template file")
+    parser.add_argument('-s', '--stack', action='store', dest='stack_name', help="Stack name")
+    results = parser.parse_args()
+
+    if not vars(results):
+        parser.print_help()
+        sys.exit(0)
+
     s = Stacker('us-east-1')
-    stacks = s.list_stacks()
-    print(stacks)
+    if parser.list:
+        stacks = s.list_stacks()
+        print(stacks)
+    elif parser.create:
+        if parser.template_file:
+            template_url = s.upload_(template(parser.template_file))
+            stacks = s.create_stack(parser.stack_name, template_url)
+        elif parser.template_url:
+            stacks = s.create_stack(parser.stack_name, template_url)
+    elif parser.delete:
+        stacks = s.delete_stack(parser.stack_name)
+    else:
+        parser.print_help()
 
     # upload local file to s3 bucket and then reference the file via s3 web url
     #template = "https://s3.amazonaws.com/cf-templates-158gc87u1eu1y-us-east-1/ec2_goatherding_us-east.yaml"
